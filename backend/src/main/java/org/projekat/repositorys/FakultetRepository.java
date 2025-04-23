f28b9f3ff4e8c50846ae8a03b48fd3f14c791375
en-z <ognjensamsal@icloud.com>
2025-04-23 12:07:21 +0200

    dodat fakultet controller

---
+++ backend/src/main/java/org/projekat/repositorys/FakultetRepository.java
@@ -0,0 +1,7 @@
+package org.projekat.repositorys;
+
+import org.projekat.model.Fakultet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FakultetRepository extends JpaRepository<Fakultet,Long> {
+}
