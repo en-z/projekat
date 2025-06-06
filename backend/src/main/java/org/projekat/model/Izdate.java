0beeb217493e15f35e6e62e55c047e2c18d0cf6a
lutkic <zoran.lutkic@gmail.com>
2025-06-06 22:46:54 +0200

    Add files via upload
    Model, book and taken

---
+++ backend/src/main/java/org/projekat/model/Izdate.java
@@ -0,0 +1,27 @@
+package org.projekat.model.biblioteka;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.projekat.model.Osoba;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Izdate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "osoba_id",referencedColumnName = "user_id")
+    private Osoba osoba;
+    @ManyToOne
+    @JoinColumn(name = "knjiga_id",referencedColumnName = "id")
+    private Knjiga knjiga;
+    private LocalDate datumIzdavanja;
+    private LocalDate datumVracanja;
+}
---
+++ backend/src/main/java/org/projekat/model/Knjiga.java
@@ -0,0 +1,23 @@
+package org.projekat.model.biblioteka;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Knjiga {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String naziv;
+    private String kategorija;
+    private String opis;
+    private String autor;
+    private int kolicina;
+}
