6164524fdf4d8c0d25c5ab48fa7444890107fca1
en-z <ognjensamsal@icloud.com>
2025-07-17 15:46:55 +0200

    @data

--- osoblje/src/main/java/com/example/service/osoblje/models/Osoblje.java
+++ osoblje/src/main/java/com/example/service/osoblje/models/Osoblje.java
@@ -1,9 +1,17 @@
 package com.example.service.osoblje.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Osoblje {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
